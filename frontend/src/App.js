5b5e8c02cb8eb2379d196284a610634c01714a22
IT21340796 <it21340796@my.sliit.lk>
2024-03-03 21:31:05 +0530

    add files

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <html>
+      <head>
+         <title>LINGO</title>
+      </head>
+      <body>
+        <h2>Welcome to Lingo!</h2>
+      </body>
+    </html>
+  );
+}
+
+export default App;
