a4a31a3fbe76a6cb6390dcea5d64fa22a6cef32c
Aneeq <aneeqshaffy7@gmail.com>
2024-03-17 09:16:58 +0000

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,25 @@
 import './App.css';
 
-//emoji to text translater
-
-
-
-
-//emoji to text translater
+// emoji to text translater
 import Header from './component/Navbar/Header';
 import Addemoji from './component/EmojiText/Addemoji';
-import { AllEmojiText } from './component/EmojiText/AllEmojiText';
-import EmojiText from './component/EmojiText/EmojiText'
+// import { AllEmojiText } from './component/EmojiText/AllEmojiText';
+import EmojiText from './component/EmojiText/EmojiText';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div>
-        <Header />
+        { <Header /> }
         <Routes>
-       {/*emoji to text translater*/}
-     
-        
-        <Route path='/add' element={<Addemoji />} />
-        <Route path='/' element={<AllEmojiText />} />
-        <Route path='emojiText' element={<EmojiText />} />
-
-       
-
+          {/* {emoji to text translater} */}
+          {/* Commenting out the route for AllEmojiText */}
+          {/* <Route path='/' element={<AllEmojiText />} /> */}
+          
+          <Route path='/add' element={<Addemoji />} />
+          <Route path='emojiText' element={<EmojiText />} />
         </Routes>
-
       </div>
     </Router>
   );
