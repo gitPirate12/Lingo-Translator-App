e0075603771088011b4bd170f31110781239f493
gitPirate12 <aneeqshaffy7@gmail.com>
2024-03-18 18:19:51 +0000

    Update User.js
    test

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true }
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
 });
 
-const User = mongoose.model('userInfoDetail', userSchema);
+userSchema.pre("save", async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 8)
+    }
+    next();
+});
+
+const User = mongoose.model("userInfo", userSchema);
 
 module.exports = User;
