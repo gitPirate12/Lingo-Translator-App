638af63b51191496d4f9b5e82b70fba40c1bb8dc
gitPirate12 <aneeqshaffy7@gmail.com>
2024-05-07 03:54:29 +0100

    chanegs the user controler and every thign cuz geeth is not doing his work

---
+++ backend/controllers/userController.js
@@ -0,0 +1,74 @@
+const User = require('../models/userModel');
+const mongoose = require('mongoose');
+
+
+// Login user
+const loginUser = async (req, res) => {
+    res.json({mssg: 'login user'})
+  }
+
+// Signup user
+const signupUser = async (req, res) => {
+    res.json({mssg: 'signup user'})
+  }
+
+
+// Get user profile
+const getUserProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+};
+
+// Delete user profile
+const deleteUserProfile = async (req, res) => {
+    try {
+        await User.findByIdAndRemove(req.user.id);
+        res.json({ msg: 'User profile deleted' });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+};
+
+// Update user profile
+const updateUserProfile = async (req, res) => {
+    try {
+        const { firstName, lastName } = req.body;
+
+        // Retrieve user from database
+        const user = await User.findById(req.user.id);
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Update user details
+        if (firstName) user.firstName = firstName;
+        if (lastName) user.lastName = lastName;
+
+        // Save updated user details
+        await user.save();
+
+        res.json({ msg: 'User profile updated successfully', user });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+};
+
+
+module.exports = {
+    loginUser,
+    signupUser,
+    getUserProfile,
+    deleteUserProfile,
+    updateUserProfile
+}
---
+++ backend/models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    city: {
+        type: String
+    }
+});
+
+//static sign up method
+
+
+
+
+
+
+module.exports = mongoose.model('userInfoDetail', userSchema);
