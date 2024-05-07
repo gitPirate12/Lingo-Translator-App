63c2e9e3c420d39fed88c7cfd3283338f282012d
gitPirate12 <aneeqshaffy7@gmail.com>
2024-05-07 05:12:50 +0100

    added signing token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,10 @@
 const User = require('../models/userModel');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+  }
 
 
 // Login user
@@ -13,8 +18,11 @@ const signupUser = async (req, res) => {
   
     try {
       const user = await User.signup(email, password)
+
+      // create a token
+      const token = createToken(user._id)
   
-      res.status(200).json({email, user})
+      res.status(200).json({email, token})
     } catch (error) {
       res.status(400).json({error: error.message})
     }
